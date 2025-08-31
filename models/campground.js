7162dc89f5c83fe569269f3a0ee988f1e7263189
Raul Rooso <rooso.raul@gmail.com>
2025-08-31 17:30:39 +0300

    Adding the reviews

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const { campgroundSchema } = require('../schemas');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -7,6 +9,31 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review',
+        }
+    ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Campground', CampgroundSchema);
+
+
+// db.reviews.deleteMany({
+//   _id: { $in: [
+//     ObjectId('68b45812cd727c3df02c30ae'),
+//     ObjectId('68b45816cd727c3df02c30b5'),
+//     ObjectId('68b45819cd727c3df02c30bb')
+//   ]}
+// })
