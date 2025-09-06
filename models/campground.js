fcd0e3bf3576fa7c5ca2b8bd7f47294622296b4b
Raul Rooso <rooso.raul@gmail.com>
2025-09-06 11:57:54 +0300

    Basic Authorization

--- models/campground.js
+++ models/campground.js
@@ -9,12 +9,16 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Review',
         }
-    ]
+    ],
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
@@ -25,7 +29,7 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
             }
         })
     }
-})
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
 
--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '68babc721a1517cbf45366ab', //must add later currently miki
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
